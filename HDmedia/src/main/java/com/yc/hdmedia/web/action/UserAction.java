00399907e407afbf28b370b37b79d07c0eee2ae4
a312928858 <312928858@qq.com>
2016-05-28 17:03:03 +0800

    fff

--- HDmedia/src/main/java/com/yc/hdmedia/web/action/UserAction.java
+++ HDmedia/src/main/java/com/yc/hdmedia/web/action/UserAction.java
@@ -7,18 +7,29 @@ import org.apache.struts2.interceptor.SessionAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ModelDriven;
 import com.yc.hdmedia.entity.User;
 import com.yc.hdmedia.service.UserService;
+import com.yc.hdmedia.utils.VerifyCode;
 
 @Controller("userAction")
 public class UserAction implements ModelDriven<User>,SessionAware{
+	private VerifyCode vc=new VerifyCode();
+	private String email;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Autowired
+	//private JavaMailSender javaMailSender; //邮件发送对象
+
 	private User user;
 	@Autowired
 	private UserService userService;
 	private Map<String, Object> session;
 	
-	
+	//用户名注册
 	public String register(){
 		LogManager.getLogger().debug("注册...");
 		int result=userService.addUser(user);
@@ -26,6 +37,25 @@ public class UserAction implements ModelDriven<User>,SessionAware{
 			return "register";
 			}
 	
+	/*//邮箱注册
+	public String register2(){
+		SimpleMailMessage smm=new SimpleMailMessage();
+		smm.setFrom("studymail_test@163.com");
+		smm.setTo(user.getYhemail());
+		smm.setSubject("验证码");
+		String uuid=vc.code();
+		ActionContext.getContext().getSession().put("user",user);
+		ActionContext.getContext().getSession().put("code", uuid);
+		smm.setText(uuid);
+		javaMailSender.send(smm);
+		int result=userService.addUser(user);
+		if(result>0){
+			return  "tiaozhuan";
+		}
+		return "false";
+		
+	}
+	*/
 	public String login(){
 		LogManager.getLogger().debug("login登陆操作...");
 		User users=userService.login(user);
